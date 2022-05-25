62063b639ca8fdd141996a32bcd13d8c54f12606
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-25 15:24:44 -0400

    add Player struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod map;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
