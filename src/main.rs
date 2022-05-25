d4844b6c29527bf487101cf5c2fe28705e250ece
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-25 00:36:59 -0400

    set up root level prelude

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-use bracket_lib::prelude::*;
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50().with_title("Dungeon Crawl").build()?;
