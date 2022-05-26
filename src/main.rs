5f311b27cdd2ffee52678749f670d7ddc09c8467
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-26 14:13:16 -0400

    set up method to create dungeon rooms

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod map;
+mod map_builder;
 mod player;
 
 mod prelude {
@@ -8,6 +9,7 @@ mod prelude {
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
     pub use crate::player::*;
 }
 
